c38b691dade3a5fc44cbcc73433b0f6cd3a0278d
fvdawang <francisison18@gmail.com>
2026-05-20 12:17:11 +0800

    merge fix

--- mobile/lib/features/appointments/pages/client_appointments_page.dart
+++ mobile/lib/features/appointments/pages/client_appointments_page.dart
@@ -339,9 +339,9 @@
 //     );
 //   }
 // }
+//test test
 
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile_app/app/theme.dart';
